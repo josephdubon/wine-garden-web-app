e0ebcf92ce3e8796d94fdffdfa2c62c23676cba6
Joseph Dubon <josephdubon@pm.me>
2022-07-30 11:37:24 -0700

    removed starter code, added hero component with some intro text

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
 import * as React from 'react'
+import {styled} from '@mui/material/styles'
 import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import ProTip from '../src/ProTip'
-import Link from '../src/Link'
-import Copyright from '../src/Copyright'
+import Hero from '../components/Hero'
+import {Grid, Paper} from '@mui/material'
+
+const Item = styled(Paper)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}))
+
 
 export default function Index() {
-    return (
-        <Container maxWidth='sm'>
-            <Box sx={{my: 4}}>
-                <Typography variant='h4' component='h1' gutterBottom>
-                    Wine Garden
-                </Typography>
-                <Link href={'/about'} color='secondary'>
-                    Go to the about page
-                </Link>
-                <ProTip/>
-                <Copyright/>
-            </Box>
-        </Container>
-    )
+    return (<>
+        {/* Video Hero */}
+        <Hero heroVideo={'/media/video/wine-garden-animation.mp4'}/>
+    </>)
 }
