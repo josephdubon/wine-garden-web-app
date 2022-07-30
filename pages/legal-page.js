12ff68d3e12158006f79da3fc45b4a79701bd424
Joseph Dubon <josephdubon@pm.me>
2022-07-30 14:42:22 -0700

    renamed about to legal-page and added terms of use and services

--- pages/legal-page.js
+++ pages/legal-page.js
@@ -3,22 +3,22 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import ProTip from '../src/ProTip'
+import TermsOfUseAndService from '../src/TermsOfUseAndService'
 import Link from '../src/Link'
 import Copyright from '../src/Copyright'
 
-export default function About() {
+export default function LegalPage() {
     return (
-        <Container maxWidth='sm'>
+        <Container maxWidth='md'>
             <Box sx={{my: 4}}>
                 <Typography variant='h4' component='h1' gutterBottom>
-                    Wine Garden
+                    Wine Garden Legal Page
                 </Typography>
+
                 <Button variant='contained' component={Link} noLinkStyle href='/'>
                     Go to the main page
                 </Button>
-                <ProTip/>
-                <Copyright/>
+                <TermsOfUseAndService/>
             </Box>
         </Container>
     )
